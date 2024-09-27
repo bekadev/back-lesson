9efac0ade5e6a079a2ad3ecd53dda4ba7be53db7
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-09-27 15:17:37 +0500

    updated endpoint

--- src/features/posts/index.ts
+++ src/features/posts/index.ts
@@ -4,7 +4,7 @@ import {getPostsController} from '../posts/controllers/getPostsController'
 import {findPostController} from '../posts/controllers/findPostController'
 import {delPostController} from '../posts/controllers/delPostController'
 import {putPostController} from '../posts/controllers/putPostController'
-import {postValidators} from '../posts/middlewares/postValidators'
+import {postValidators, shortDescriptionValidator, titleValidator} from '../posts/middlewares/postValidators'
 import {adminMiddleware} from "../../middleware/auth-middleware";
 import {inputCheckErrorsMiddleware} from "../../middleware/input-check-errors-middleware";
 
@@ -14,6 +14,7 @@ postsRouter.post('/', ...postValidators, createPostController)
 postsRouter.get('/', getPostsController)
 postsRouter.get('/:id', findPostController)
 postsRouter.delete('/:id', adminMiddleware, delPostController)
-postsRouter.put('/:id', inputCheckErrorsMiddleware, adminMiddleware, putPostController)
+postsRouter.put('/:id', titleValidator,
+  shortDescriptionValidator,inputCheckErrorsMiddleware, adminMiddleware, putPostController)
 
 // не забудьте добавить роут в апп
