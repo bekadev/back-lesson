d5eaea2f341d1d4ad6a55d6461e9969030ba6810
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-10-25 13:40:41 +0500

    updated endpoint

--- src/features/posts/middlewares/postValidators.ts
+++ src/features/posts/middlewares/postValidators.ts
@@ -1,15 +1,7 @@
-import {NextFunction, Request, Response} from 'express'
 import {body} from 'express-validator'
 import {adminMiddleware} from "../../../middleware/auth-middleware";
-// import {InputValidationMiddleware} from "../../../middleware/input-validation-middleware";
 import {inputCheckErrorsMiddleware} from "../../../middleware/input-check-errors-middleware";
 import {blogsRepository} from '../../blogs/blogsRepository'
-import {postsRepository} from '../postsRepository'
-
-// title: string // max 30
-// shortDescription: string // max 100
-// content: string // max 1000
-// blogId: string // valid
 
 export const titleValidator = body('title').trim().isString().isLength({min: 1, max: 30}).withMessage('title error')
 export const shortDescriptionValidator = body('shortDescription').trim().isString().isLength({
@@ -26,32 +18,14 @@ export const blogIdValidator = body('blogId')
 	if (!blog) {
 		throw new Error('no blog');
 	}
-	return true; // Return true if the blog exists
+	return true;
 }).withMessage('blogId error');
-
-
-export const findPostValidator = (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
-	const post = postsRepository.find(req.params.id)
-	if (!post) {
-
-		res
-		.status(404)
-		.json({})
-		return
-	}
-
-	next()
-}
-
 
 export const postValidators = [
 	adminMiddleware,
 	titleValidator,
 	shortDescriptionValidator,
 	contentValidator,
-	inputCheckErrorsMiddleware,
 	blogIdValidator,
-
-	// findPostValidator,
-	// InputValidationMiddleware,
+	inputCheckErrorsMiddleware,
 ]
