6344ad5b604d44b94ce5c2507cbd1d6fbe4618c6
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-10-25 13:41:55 +0500

    updated endpoint

--- src/features/posts/middlewares/postValidators.ts
+++ src/features/posts/middlewares/postValidators.ts
@@ -1,4 +1,4 @@
-import {body} from 'express-validator'
+import {body, param} from 'express-validator'
 import {adminMiddleware} from "../../../middleware/auth-middleware";
 import {inputCheckErrorsMiddleware} from "../../../middleware/input-check-errors-middleware";
 import {blogsRepository} from '../../blogs/blogsRepository'
@@ -11,6 +11,17 @@ export const shortDescriptionValidator = body('shortDescription').trim().isStrin
 export const contentValidator = body('content').isString().withMessage('not string')
 .trim().isLength({min: 1, max: 1000}).withMessage('more then 1000 or 0')
 export const blogIdValidator = body('blogId')
+.isString().withMessage('blogId must be a string')
+.trim()
+.custom(async (blogId) => {
+	const blog = await blogsRepository.find(blogId);
+	if (!blog) {
+		throw new Error('no blog');
+	}
+	return true;
+}).withMessage('blogId error');
+
+export const blogIdValidatorParams = param('blogId')
 .isString().withMessage('blogId must be a string')
 .trim()
 .custom(async (blogId) => {
